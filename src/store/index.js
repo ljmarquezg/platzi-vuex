26550f174bedaef6abe5a797919ae0d1184955a5
Luis Marquez <ljmarquezg@gmail.com>
2024-05-19 20:31:56 +0200

    12 - namespace

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,18 @@
 import { createStore } from 'vuex'
 import profile from "@/store/modules/profile";
+import { COMMIT_UPDATE_STATUS } from "@/common/mutation-types";
 
 const store = createStore({
+  state() {
+    return {
+      status: null,
+    }
+  },
+  mutations: {
+    [COMMIT_UPDATE_STATUS](state, newStatus) {
+      state.status = newStatus;
+    }
+  },
   modules: {
     profile
   }
